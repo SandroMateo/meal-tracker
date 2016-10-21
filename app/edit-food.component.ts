688d30c930c08fb0526e84dce3e2462a8f457b48
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-10-21 08:17:29 -0700

    add form to edit-food template using a two-way bind

--- app/edit-food.component.ts
+++ app/edit-food.component.ts
@@ -4,10 +4,25 @@ import { Food } from './food.model';
 @Component ({
   selector: 'edit-food',
   template: `
-
+  <div *ngIf="show">
+    <h2>New Food</h2>
+    <div class="form-group">
+      <label for="name">Food: </label>
+      <input [(ngModel)]="food.name" class="form-control" type="text">
+    </div>
+    <div class="form-group">
+      <label for="details">Details: </label>
+      <input [(ngModel)]="food.details" class="form-control" type="text">
+    </div>
+    <div class="form-group">
+      <label for="calories">Calories: </label>
+      <input [(ngModel)]="food.calories" class="form-control" type="text">
+    </div>
+    <button class="btn" (click)="finishEdit()">Finish</button>
+  </div>
   `
 })
 
 export class EditFoodComponent {
-  
+
 }
--- app/new-food.component.ts
+++ app/new-food.component.ts
@@ -7,15 +7,15 @@ import { Food } from './food.model';
     <div *ngIf="show">
       <h2>New Food</h2>
       <div class="form-group">
-        <label for="name">Food</label>
+        <label for="name">Food: </label>
         <input #name class="form-control" type="text">
       </div>
       <div class="form-group">
-        <label for="details">Details</label>
+        <label for="details">Details: </label>
         <input #details class="form-control" type="text">
       </div>
       <div class="form-group">
-        <label for="calories">Calories</label>
+        <label for="calories">Calories: </label>
         <input #calories class="form-control" type="text">
       </div>
       <button class="btn" (click)="addFood(name.value, details.value, calories.value);
