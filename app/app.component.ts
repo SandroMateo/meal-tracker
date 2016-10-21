0d794e08814f15fc66290015578aa6dcd906aa59
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-10-20 18:53:56 -0700

    import FormsModule into app.module and create an array of Foods in app.component hard coded with 4 foods for testing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Food } from './food.model';
 
 @Component({
   selector: 'my-app',
   template: `
   <h1>Meal Tracker</h1>
+  <new-food
+  ></new-food>
   `
 })
 
 export class AppComponent {
-
+  allFoods: Food[] = [
+    new Food("Salad", "The same big salad I eat every day for lunch", 350),
+    new Food("Trail Mix", "handful of almonds, walnuts, and raisins", 250),
+    new Food("Burrito", "chicken burrito with guacamole, sour cream, beans, rice, spinach, salsa, and cheese", 600),
+    new Food("Chocolate Hazelnut Scone", "really sugary, but delicious", 100)
+  ];
 }
