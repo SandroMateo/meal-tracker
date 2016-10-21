02f8e4b217220ef4aa9930b27d18f58ad1ef0f89
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-10-21 09:03:19 -0700

    add radio buttons to food-filter template, allowing user to choose between all, low calorie food, or high calorie food

--- app/food-filter.component.ts
+++ app/food-filter.component.ts
@@ -4,10 +4,27 @@ import { Food } from './food.model';
 @Component ({
   selector: 'food-filter',
   template: `
-
+    <h4>Filters</h4>
+    <div class="form-group" (change)="calorieChanges($event.target.value)">
+      <div class="radio">
+        <label><input type=radio name="calories" value="all" checked>
+          All
+        </label>
+      </div>
+      <div class="radio">
+        <label><input type=radio name="calories" value="low">
+          Low Calorie Food
+        </label>
+      </div>
+      <div class="radio">
+        <label><input type=radio name="calories" value="high">
+          High Calorie Food
+        </label>
+      </div>
+    </div>
   `
 })
 
 export class FoodFilterComponent {
-  
+
 }
