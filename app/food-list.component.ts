3faaebaf789474239aa80a3f497a169b19bc54a9
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-10-20 19:42:45 -0700

    add content to the template of the food-list component which will cycle through the allFoods array, passing each food into the food-display component to be displayed. Also add an edit button without functionality

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -2,12 +2,17 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Food } from './food.model';
 
 @Component ({
-  selector: 'food-list'
+  selector: 'food-list',
   template: `
-
+    <div *ngFor="let currentFood of childAllFoods">
+      <food-display
+        [food]="currentFood"
+      ></food-display>
+      <button class="btn" (click)="editFood(currentFood)">Edit</button>
+    </div>
   `
 })
 
 export class FoodListComponent {
-  
+
 }
