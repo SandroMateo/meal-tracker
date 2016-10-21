cc835838e24e62fbe53a63c5c33ab1cd3cfd3cf8
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-10-20 19:59:53 -0700

    add content to template of food-display component, listing out name, details, and calories of inputted Food object

--- app/food-display.component.ts
+++ app/food-display.component.ts
@@ -4,7 +4,9 @@ import { Food } from './food.model';
 @Component ({
   selector: 'food-display',
   template: `
-
+    <h2> {{ food.name }} </h2>
+    <p> {{ food.details }} </p>
+    <p> {{ food.calories }} </p>
   `
 })
 
