4d7409a7cfad5340e6f7ef99ea0911fec6c27990
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-10-20 19:12:35 -0700

    add content into template of the new-food component, allowing user to fill out a form to create new food object

--- app/new-food.component.ts
+++ app/new-food.component.ts
@@ -1,13 +1,31 @@
 import { Component, Output, EventEmitter } from '@angular/core';
-import { Food } from './food.model.ts';
+import { Food } from './food.model';
 
 @Component ({
   selector: 'new-food',
   template: `
-
+    <div *ngIf="show">
+      <h2>New Food</h2>
+      <div class="form-group">
+        <label for="name">Food</label>
+        <input #name class="form-control" type="text">
+      </div>
+      <div class="form-group">
+        <label for="details">Details</label>
+        <input #details class="form-control" type="text">
+      </div>
+      <div class="form-group">
+        <label for="calories">Calories</label>
+        <input #calories class="form-control" type="text">
+      </div>
+      <button class="btn" (click)="addFood(name.value, details.value, calories.value);
+      name.value = '';
+      details.value = '';
+      calories.value = '';">Add Food</button> 
+    </div>
   `
 })
 
 export class NewFoodModule {
-  
+
 }
