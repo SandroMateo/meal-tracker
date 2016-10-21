5240bd728083287f29f20cd04db42ea21eaca6a5
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-10-20 19:00:26 -0700

    add beginning of a new-food component that will allow user to fill out a form to create a new food object

---
+++ app/new-food.component.ts
@@ -0,0 +1,13 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Food } from './food.model.ts';
+
+@Component ({
+  selector: 'new-food',
+  template: `
+
+  `
+})
+
+export class NewFoodModule {
+  
+}
